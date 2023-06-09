3d04c7588971237f7c4b9ec2b638f6d6dc912994
adeprastyo <adeprastyo101@gmail.com>
2023-06-09 14:38:17 +0700

    learn custom theme and custom style component mui

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Notes from "./Pages/Notes";
 import Create from "./Pages/Create";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { blue } from "@mui/material/colors";
+
+/*
+  createTheme() adalah cara untuk mengubah/override default theme dari Material UI
+  ThemeProvider adalah cara untuk memakai custom theme mana yang mau dipakai.
+  di bawah ini saya mengubah default theme untuk warna palete primary secondary dan mengubah
+  jenis font dan fontweight
+*/
+const custTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#00ba06",
+    },
+    secondary: blue,
+  },
+  typography: {
+    fontFamily: "Lato",
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightBold: 700,
+  },
+});
+
+const defaultTheme = createTheme(); // ini adalah cara jika ingin menggunakan kembali defaultTheme
 
 function App() {
+  const theme = custTheme;
   return (
-    <Routes>
-      <Route exact path="/" element={<Notes />} />
-      <Route path="/create" element={<Create />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route exact path="/" element={<Notes />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,4 +1,5 @@
-/*  cara import => import Typography from "@mui/material/Typography"
+/*  
+    cara import => import Typography from "@mui/material/Typography"
     kita bisa mendefinisikan nama component typography dengan bebas, 
     contoh misal kita ingin menamai component typography dengan "Teks"
     kita bisa menuliskannya seperti ini,
@@ -7,23 +8,37 @@
     sedangkan cara import => import { Typography } from "@mui/material/Typography" 
     kita tidak bisa mendefinisikan nama component seperti cara pertama, tapi harus ditulis
     spesifik nama component yang ada di materialUI
+
+    styled() adalah cara untuk mengcustom style pada component yang kita ingin custom 
+    style nya.
 */
 
 import { Container, Typography, Button } from "@mui/material";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 import { styled } from "@mui/system";
 
+/* 
+  contoh di bawah ini adalah saya mengcustom style component Button dari bawaan Mui.
+  saya ingin agar button nya saat di hover mengubah warna button menjadi white dan warna tulisan nya 
+  menjadi #00ba06 dan juga mengubah warna tulisan saat tidak di hover menjadi white.
+*/
 const CustButton = styled(Button)({
   "&:hover": {
     backgroundColor: "white",
-    color: "blue",
+    color: "#00ba06",
   },
+  color: "white",
 });
 
 export default function Create() {
   return (
     <Container>
-      <Typography variant="h6" component="h1" color="textSecondary">
+      <Typography
+        variant="h6"
+        component="h1"
+        color="textPrimary"
+        fontWeight="bold"
+      >
         Create a New Note
       </Typography>
       <CustButton
